8279cd1ee455484f33e3d3a9a7ae4409bfc4d2c2
AntonThoresson <anton.thoresson@hotmail.com>
2023-02-01 18:02:36 +0100

    ticket-1 done (landingpage)

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,31 +1,32 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
-  import Home from '/Users/alexanderforsanker/AdvancedWeb/ShopTech/frontend/src/lib/Home.svelte'
-	import SellTech from '/Users/alexanderforsanker/AdvancedWeb/ShopTech/frontend/src/lib/SellTech.svelte'
-	import FAQ from '/Users/alexanderforsanker/AdvancedWeb/ShopTech/frontend/src/lib/FAQ.svelte'
+  import Home from "./lib/Home.svelte"
+  import SellTech from "./lib/SellTech.svelte"
+  import FAQ from "./lib/FAQ.svelte"
 </script>
 
 <Router>
   <header>
     <nav
-      class="navbar bg-dark navbar-expand-lg bg-body-tertiary fixed-top"
-      data-bs-theme="dark">
+      class="navbar bg-dark navbar-expand-lg bg-body-tertiary"
+      data-bs-theme="dark"
+    >
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <Link to="/" class="nav-link active" aria-current="page">Home</Link>
+              <Link to="#" class="nav-link active" aria-current="page">Home</Link>
             </li>
             <li class="nav-item">
-              <Link to="/sellTech" class="nav-link active" aria-current="page">Sell Tech</Link>
+              <Link class="nav-link" to="#">Sell Tech</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <Link class="nav-link" to="#">Pricing</Link>
             </li>
             <li class="nav-item">
-              <Link to="/faq" class="nav-link active" aria-current="page">FAQ</Link>
+              <Link class="nav-link" to="#">FAQ</Link>
             </li>
-            <a href="#" class="navbar-brand logo">ShopTech</a>
+            <Link to="#" class="navbar-brand logo">ShopTech</Link>
           </ul>
           <form class="d-flex" role="search">
             <input
@@ -40,7 +41,7 @@
         </div>
       </div>
     </nav>
-  </header>
+  </header> 
 
   <main>
     <p>&nbsp; </p>
@@ -49,36 +50,78 @@
     <Route path="/sellTech" component={SellTech}></Route>
 		<Route path="/faq" component={FAQ}></Route>
   </main>
-  <!-- <main>
-  <h1>Test title</h1> 
-    <Route path="/about" component={SellTech}></Route>
-		<Route path="/posts" component={FAQ}></Route>
-		<Route path="/" component={Home}></Route>
-  </main>
--->
-  <footer class="bg-body-teriary text-body-secondary pt-5 pb-4">
-<!--  <div class="container text-center text-md.left">
+  
+  <footer class="bg-dark-subtle text-body-secondary pt-5 pb-4 bottom-footer">
+    <div class="container text-center text-md-left">
       <div class="row text-center text-md-left">
         <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-          <h5 class="fw-bold mb-4 text-dark">ShopTech</h5>
-          <p>Our mission is to be the leading tech reseller in Scandinavia. 
+          <h5 class="fw-bold mb-4 text-start" >ShopTech</h5>
+          <p class="text-start">
+            Our mission is to be the leading tech reseller in Scandinavia. 
 
             We started our mission our of frustration because of the high prices on consumer electronics,
-            and the lack of buyer protection when buying used and refurbished tech.
+             and the lack of buyer protection when buying used and refurbished tech.
             
             We can provide both new and used electronics at a fair price. We can also provide you with a 
-            one year warranty and free repairs. 
+            one year warranty and free repairs.
           </p>
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h5 class="fw-bold mb-4 text-dark">Products</h5>
-            <p>
-              <Link to="#" class="text-body-secondary" style="text-decoration: none;">IPhone</Link>
-              <Link to="#" class="text-body-secondary" style="text-decoration: none;">IPad</Link>
-              <Link to="#" class="text-body-secondary" style="text-decoration: none;">Macbook</Link>
-            </p>
-          </div>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="fw-bold mb-4 text-start">Products</h5>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">iPhone</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">iPad</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">Macbook</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">Android</Link>
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="fw-bold mb-4 text-start">Menu</h5>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">Home</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">Sell Tech</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">Pricing</Link>
+          </p>
+          <p class="text-start">
+            <Link to="#" class="text-body-secondary" style="text-decoration: none;">FAQ</Link>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mt-3">
+          <h5 class="fw-bold mb-4 text-start">Contact</h5>
+          <p class="text-start">
+            <i class="bi bi-house-fill"></i> Gjuterigatan 5, JKPG, 55318, SE
+          </p>
+          <p class="text-start">
+            <i class="bi bi-envelope-fill"></i> office@shoptech.com
+          </p>
+          <p class="text-start">
+            <i class="bi bi-telephone-fill"></i> 08-446 800 16
+          </p>
+          <p class="text-start">
+            <i class="bi bi-printer-fill"></i> 01 335 633 77
+          </p>
         </div>
       </div>
-    </div> -->  
+      <hr class="mb-4">
+      <div class="row align-items-center">
+        <div class="col-md-7 col-lg-8">
+          <p>Copyright &#169 2023 All rights reserved by: 
+            <Link to="#" style="text-decoration: none;">
+              <strong class="text-body-secondary">ShopTech</strong>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   </footer>
 </Router>
