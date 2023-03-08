03db42ee9b8102c15c245e83a897aa67be69729c
foal20ym <alexander-forsanker2011@hotmail.com>
2023-03-08 11:42:33 +0100

    Created the tables in the database.

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,8 +1,32 @@
-CREATE TABLE humans (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(50)
+CREATE TABLE accounts (
+	accountID INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(50),
+	password VARCHAR(255),
+	address VARCHAR(50),
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	phoneNumber VARCHAR(50),
+	createdAt VARCHAR(50)
 );
 
-INSERT INTO humans (name) VALUES ('Alice');
-INSERT INTO humans (name) VALUES ('Fatima');
-INSERT INTO humans (name) VALUES ('Conrad');
+CREATE TABLE adverts (
+	advertID INT PRIMARY KEY AUTO_INCREMENT,
+	category VARCHAR(50),
+	title VARCHAR(50),
+	price INT,
+	description VARCHAR(50),
+	img_src VARCHAR(50),
+	createdAt VARCHAR(50),
+	accountID INT,
+	FOREIGN KEY (accountID) REFERENCES accounts(accountID)
+);
+
+CREATE TABLE comments (
+	commentID INT PRIMARY KEY AUTO_INCREMENT,
+	content VARCHAR(50),
+	createdAt VARCHAR(50),
+	accountID INT,
+	advertID INT,
+	FOREIGN KEY (accountID) REFERENCES accounts(accountID),
+	FOREIGN KEY (advertID) REFERENCES adverts(advertID)
+);
